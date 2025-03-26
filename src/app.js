6bfb99c337a12d8efa3908092f354f90d3bba134
ImJAraya <fabiarayamontero@gmail.com>
2025-03-26 16:42:02 -0600

    Aplicando mejoras al codigo

--- src/app.js
+++ src/app.js
@@ -1,47 +1,29 @@
-let symbol = [1, 2, 3, 4];
-let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-let carta = document.getElementById("random");
-let formaCarta = carta.querySelectorAll(".col");
-
-let cartaEscogida = [(Math.floor(Math.random() * symbol.length)) + 1, (Math.floor(Math.random() * num.length)) + 1];
-
-
-function presentCartaAlHTML() {
-  let armarCartaHead= formaCarta[0];
-  let armarCartaNum= formaCarta[1];
-  let armarCartaFooter= formaCarta[2];
-  switch (cartaEscogida[0]) {
-    case 1:
-      armarCartaHead.parentNode.innerHTML= `<div class="col d-flex justify-content-start spade">♠ </div>`
-      armarCartaNum.parentNode.innerHTML= `<div class="col d-flex justify-content-center num">${cartaEscogida[1]}</div>`
-      armarCartaFooter.parentNode.innerHTML= `<div class="col d-flex justify-content-end spade">♠</div>`
-      break;
-    case 2:
-      armarCartaHead.parentNode.innerHTML= `<div class="col d-flex justify-content-start club">♣ </div>`
-      armarCartaNum.parentNode.innerHTML= `<div class="col d-flex justify-content-center num">${cartaEscogida[1]}</div>`
-      armarCartaFooter.parentNode.innerHTML=`<div class="col d-flex justify-content-end club">♣</div>`
-      break;
-    case 3:
-      armarCartaHead.parentNode.innerHTML= `<div class="col d-flex justify-content-start heart">♥ </div>`
-      armarCartaNum.parentNode.innerHTML= `<div class="col d-flex justify-content-center num">${cartaEscogida[1]}</div>`
-      armarCartaFooter.parentNode.innerHTML=`<div class="col d-flex justify-content-end heart">♥</div>`
-      break;
-    case 4:
-      armarCartaHead.parentNode.innerHTML= `<div class="col d-flex justify-content-start diamond">♦ </div>`
-      armarCartaNum.parentNode.innerHTML= `<div class="col d-flex justify-content-center num">${cartaEscogida[1]}</div>`
-      armarCartaFooter.parentNode.innerHTML=`<div class="col d-flex justify-content-end diamond">♦</div>`
-      break;
-    default:
-      break;
-  }
-
-
-}
-
-
-window.onload = function () {
-  presentCartaAlHTML()
-  console.log()
-  console.log(cartaEscogida);
+let symbolColor = {
+  "♠": true, 
+  "♣": true, 
+  "♥": false, 
+  "♦": false
 };
+let symbol = ["♠", "♣", "♥", "♦"];
+
+
+let cartaEscogida = {
+  pinta : symbol[(Math.floor(Math.random() * symbol.length))], 
+  numero : (Math.floor(Math.random() * 12)) + 1
+};
+
+let cardHead = document.getElementById("cardHead")
+cardHead.innerHTML=cartaEscogida.pinta
+symbolColor[cartaEscogida.pinta] ? cardHead.classList.add("negro"): cardHead.classList.add("rojo")
+
+let cardNum = document.getElementById("cardNum")
+cardNum.innerHTML=cartaEscogida.numero
+cardNum.classList.add("num")
+
+let cardFoot = document.getElementById("cardFoot")
+cardFoot.innerHTML=cartaEscogida.pinta
+symbolColor[cartaEscogida.pinta] ? cardFoot.classList.add("negro"): cardFoot.classList.add("rojo")
+
+
+// window.onload = function () {
+// };
